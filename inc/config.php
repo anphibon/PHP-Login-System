2b5c5f451bd7d2540e5742663eec52e2e28efecf
anphibon <anphibon@yahoo.com>
2019-01-03 21:46:20 -0600

    the form now works with the database

--- inc/config.php
+++ inc/config.php
@@ -3,7 +3,11 @@
 	if(!defined('__CONFIG__')){
 		exit('you do not have a config file');
 	}
+	//allow errors
+	error_reporting(-1);
+	ini_set('display_errors', 'On');
 	include_once "classes/DB.php";
+	include_once "classes/Filter.php";
 	// our config is bellow
 	$con = DB::getConnection();
 ?>
